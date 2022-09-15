e8a484eaf1a4fbd0a71a588e9404cc483aea7b63
Soumalya Mondal <soumalya.mondal6@gmail.com>
2022-09-15 21:36:23 +0530

    Create DemoNew.java

---
+++ DemoNew.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DemoNew {
+
+	@Test
+	public void test1() {
+		System.out.println("in test1");
+	}
+	@Test
+	public void test2() {
+		System.out.println("in test2");
+	}
+
+}
