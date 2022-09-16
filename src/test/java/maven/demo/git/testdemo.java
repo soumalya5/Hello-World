894dd2a977a96e3fb584c8af023c0208196c1c9e
Soumalya Mondal <soumalya.mondal6@gmail.com>
2022-09-16 21:06:50 +0530

    Create testdemo.java

---
+++ src/test/java/maven/demo/git/testdemo.java
@@ -0,0 +1,18 @@
+package maven.demo.git;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testdemo {
+
+	@Test
+	void test1() {
+		System.out.println("in test1");
+	}
+	@Test
+	void test2() {
+		System.out.println("in test2");
+	}
+
+}
