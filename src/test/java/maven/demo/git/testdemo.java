5530be166838cea515474f1cb5f54aac9ab486f6
Soumalya Mondal <soumalya.mondal6@gmail.com>
2022-09-20 00:51:36 +0530

    Update testdemo.java

--- src/test/java/maven/demo/git/testdemo.java
+++ src/test/java/maven/demo/git/testdemo.java
@@ -6,12 +6,16 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 //import java.awt.List;
 import java.util.*;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -20,17 +24,22 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 class TestDemo {
 
 	@Test
-	void whenTest() {
+	void whenTest() throws MalformedURLException {
 		System.out.println("in testdemodemo1");
 		//System.setProperty("webdriver.chrome.driver", ".\\driver\\chromedriver.exe");
-		WebDriver driver;
-		WebDriverManager.chromedriver().version("105.0.5195.102").setup();
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--headless", "--start-maximized","--ignore-certificate-errors", "--silent");
-		driver = new ChromeDriver(options);
-		
-		driver.manage().window().maximize();
+//		WebDriver driver;
+//		WebDriverManager.chromedriver().version("105.0.5195.102").setup();
+//		ChromeOptions options = new ChromeOptions();
+//		options.addArguments("--headless", "--start-maximized","--ignore-certificate-errors", "--silent");
+//		driver = new ChromeDriver(options);
+//		
+//		driver.manage().window().maximize();
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setCapability("browserName","chrome");
+		capabilities.setCapability("enableVNC", false);
+		capabilities.setCapability("enableVideo", false);
 		String url="https://www.totaljobs.com";
+		WebDriver driver=new RemoteWebDriver(new URL("http//localhost:4444/wd/hub"), capabilities);
 		driver.get(url);
 		driver.manage().window().maximize();
 		System.out.println("The page title is : " +driver.getTitle());
